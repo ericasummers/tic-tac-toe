c2c2541e913b2c786a0f63b587c64b84b8ad7aee
Erica Wright and Sarah Leahy <sarah@>
2017-01-26 16:20:40 -0800

    create initial html page with grid of columns and basic styling with javascript effect on newgame

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+function Player(mark, name) {
+    this.mark = mark;
+    this.pName = name;
+    this.pChoices = [];
+}
+
+function Board(row, column) {
+    this.rows = [];
+    this.columns = [];
+}
+
+var winningCombination = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+];
+
+Player.prototype.checkVictory = function() {
+    if (this.pChoices === winningCombination) {
+        alert("You won, congratulations!");
+    }
+}
+
+//
+//
+// function Game(playerone, playertwo, board, turn) {
+//     this.new Player = ("x", "playerOne");
+//     this.new Player = ("o", "playerTwo");
+//     this.new Board = ([1, 2, 3], [1, 2, 3]);
+//     // need two sets of brckets?
+//     this.turn: function() {
+//         Board.checkSpace(clicked box)
+//         player.mark
+//     }
+//
+// }
+$(function() {
+    $("#new-game-button").click(function() {
+        $(".grid").show();
+    });
+
+    $(".col-md-4").each(function() {
+        $(this).click(function() {
+            $("p", this).text("hello");
+            $(".col-md-4", this).off();
+        });
+    });
+});
+
+// check box allow box to be selected
+// if free and check to see
+// if the game has been won / no more moves, check
+// for user turns
